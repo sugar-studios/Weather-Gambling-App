79bbdf392c0e2cc44fe573a88c9776d99dae1129
Daniel <abcalkins8723@gmail.com>
2024-02-27 13:12:52 -0700

    added gradle dependencies

--- Weather Gambler/app/build.gradle.kts
+++ Weather Gambler/app/build.gradle.kts
@@ -90,4 +90,10 @@ dependencies {
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("com.android.volley:volley-cronet:1.2.1")
+    implementation("com.google.code.gson:gson:2.10")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
 }
